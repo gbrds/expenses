795263795ac58e9028529011e97a839098bf313d
Juss <juss-joosep.holdt@voco.ee>
2025-02-12 12:42:54 +0200

    YL4 ; 11. docs ; Form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import './assets/expenses/Expenses.css';
 import Expenses from './assets/expenses/Expenses';
+import NewExpense from './assets/newExpense/newExpense';
 
 const App = () => {
   const expenses = [
@@ -8,8 +9,14 @@ const App = () => {
     { date: new Date(2024, 1, 16), title: 'New Stuff', price: 99.99 }
   ];
 
+const addExpenseHandler = (expense) => {
+  console.log('in app.js')
+  console.log(expense)
+}
+
   return (
     <div className="App">
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
---
+++ src/assets/newExpense/expenseForm.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './expenseForm.css'
+
+const ExpenseForm = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredPrice, setEnteredPrice] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+    }
+    const priceChangeHandler = (event) => {
+        setEnteredPrice(event.target.value)
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        const expenseData = {
+            title: enteredTitle,
+            price: enteredPrice,
+            date: new Date(enteredDate)
+        }
+        props.onSaveExpenseData(expenseData)
+        setEnteredTitle('')
+        setEnteredPrice('')
+        setEnteredDate('')
+    }
+
+    return(
+        <form onSubmit={submitHandler}>
+            <div className='new-expense_controls'>
+                <div className='new-expense_control'>
+                    <label>Title</label>
+                    <input type="text"
+                        onChange={titleChangeHandler}
+                        value={enteredTitle}/>
+                </div>
+                <div className='new-expense_control'>
+                    <label>Price</label>
+                    <input type="number" min="0.01" step="0.01"
+                        onChange={priceChangeHandler}
+                        value={enteredPrice}/>
+                </div>
+                <div className='new-expense_control'>
+                    <label>Date</label>
+                    <input type="date" min="2025-02-12" max="2026-01-31"
+                        onChange={dateChangeHandler}
+                        value={enteredDate}/>
+                </div>
+            </div>
+            <div className='new-expense_actions'>
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    )
+}
+
+export default ExpenseForm
