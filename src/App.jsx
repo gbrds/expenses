5112d849f4b9bcb9216971554942ed9944a58508
Juss <juss-joosep.holdt@voco.ee>
2025-02-12 10:56:28 +0200

    YL5 ; YL7 docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,18 @@
-import './App.css'
-import './assets/Expenses.css'
-import ExpenseItem from './assets/expenseItem'
+import './App.css';
+import './assets/Expenses.css';
+import Expenses from './assets/Expenses';
 
 const App = () => {
-const expenses = [
-  {
-    date: new Date(2024, 1, 16),
-    title: 'New Book',
-    price: 30.99
-  },
-  {
-    date: new Date(2024, 1, 16),
-    title: 'New Stuff',
-    price: 99.99
-  }
-]
-
-  return(
-  <div className="App">
-    <div className='expenses'>
-    <ExpenseItem data={expenses[0]}/>
-    <ExpenseItem data={expenses[1]}/>
-    </div>
-  </div>
-  )
-}
-
-export default App
-
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+  const expenses = [
+    { date: new Date(2024, 1, 16), title: 'New Book', price: 30.99 },
+    { date: new Date(2024, 1, 16), title: 'New Stuff', price: 99.99 }
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <Expenses expenses={expenses} />
+    </div>
+  );
+};
 
-export default App*/
+export default App;
---
+++ src/assets/Expenses.jsx
@@ -0,0 +1,15 @@
+import './Expenses.css'
+import ExpenseItem from './expenseItem'
+import Card from './card'
+
+const Expenses = (props) => {
+
+    return (
+        <Card className="expenses">
+            <ExpenseItem data={props.expenses[0]}/>
+            <ExpenseItem data={props.expenses[1]}/>
+        </Card>
+    )
+}
+
+export default Expenses
