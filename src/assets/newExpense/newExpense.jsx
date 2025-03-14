f5654c940ec06dbcfb0ff865392e56fd167278f0
Juss <juss-joosep.holdt@voco.ee>
2025-03-14 10:15:46 +0200

    show/hide expenses form

--- src/assets/newExpense/newExpense.jsx
+++ src/assets/newExpense/newExpense.jsx
@@ -1,18 +1,39 @@
 import ExpenseForm from "./expenseForm"
 import './newExpenses.css'
+import React, { useState } from "react";
 
 const NewExpense = (props) => {
+    const [isFormVisible, setIsFormVisible] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
-        props.onAddExpense(expenseData)
+        props.onAddExpense(expenseData);
+        setIsFormVisible(false);
     }
+    
+    const showForm = () => {
+        setIsFormVisible(true);
+    };
+
+    const hideForm = () => {
+        setIsFormVisible(false);
+    };
 
     return(
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isFormVisible && (
+                <button onClick={showForm}>Add New Expense</button>
+            )}
+            {isFormVisible && (
+                <ExpenseForm
+                onSaveExpenseData={saveExpenseDataHandler} 
+                onCancel={hideForm}
+                />
+
+            )}
         </div>
     )
 }
